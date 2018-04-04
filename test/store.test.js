d5a84f9f605672904042df712d9d3b29e492db24
Keli Hansen <keli.p.hansen@gmail.com>
2018-04-03 17:47:59 -0700

    save method saves object in array

--- test/store.test.js
+++ test/store.test.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 const Store = require('../lib/store');
 
+
 describe('Store', () => {
     let storeInstance;
     beforeEach(() => {
@@ -15,5 +16,11 @@ describe('Store', () => {
         const newlyIdentified = storeInstance.save({});
         const idCheck = newlyIdentified._id ? true : false;
         assert.equal(idCheck, true);
+    });
+
+    it('has a save method that also adds the object to its storage list', () => {
+        const newlySaved = storeInstance.save({});
+        const storedCheck = storeInstance.list[0];
+        assert.equal(storedCheck, newlySaved);
     });
 });
