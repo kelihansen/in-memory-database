67b9cce179caa9621071ace93dda550027aa1870
Keli Hansen <keli.p.hansen@gmail.com>
2018-04-03 17:58:23 -0700

    add more interesting test object

--- test/store.test.js
+++ test/store.test.js
@@ -8,18 +8,20 @@ describe('Store', () => {
         storeInstance = new Store();
     });
 
+    const testObject = { bird: 'cardinal', color: 'red' };
+
     it('exists and has a property "list" that is an empty array', () => {
         assert.deepEqual(storeInstance.list, []);
     });
 
     it('has a save method that adds a new id to an object and returns that object', () => {
-        const newlyIdentified = storeInstance.save({});
+        const newlyIdentified = storeInstance.save(testObject);
         const idCheck = shortid.isValid(newlyIdentified._id);
         assert.equal(idCheck, true);
     });
 
     it('has a save method that also adds the object to its storage list', () => {
-        const newlySaved = storeInstance.save({});
+        const newlySaved = storeInstance.save(testObject);
         const storedCheck = storeInstance.list[0];
         assert.equal(storedCheck, newlySaved);
     });
